5529a986d883df8802b10f85aa49abd2d990ed63
Khiew Jian Bin <khiewbin@gmail.com>
2025-03-08 03:43:29 +0800

    Init

---
+++ Timer/Samples~/Examples/TimerExample.cs
@@ -0,0 +1,99 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using asim.unity.utils;
+
+public class TimerExample : MonoBehaviour
+{
+    [Header("Repeater")]
+    [SerializeField] TMP_Text durationRunning_Text;
+    [SerializeField] TMP_Text durationTime_Text;
+    [SerializeField] TMP_Text durationRuns_Text;
+    [SerializeField] Button reset_btn;
+
+    [Header("Repeater")]
+    [SerializeField] TMP_Text repeaterRunning_Text;
+    [SerializeField] TMP_Text repeaterTime_Text;
+    [SerializeField] TMP_Text repeaterRuns_Text;
+    [SerializeField] Button startstop_btn;
+
+    [Header("Interval")]
+    [SerializeField] TMP_Text intervalRunning_Text;
+    [SerializeField] TMP_Text intervalTime_Text;
+    [SerializeField] TMP_Text intervalRuns_Text;
+    [SerializeField] Button shoot_btn;
+
+    // Create new
+    Timer durationTimer;
+    Timer repeaterTimer;
+    Timer intervalTimer;
+
+    void Start()
+    {
+        durationTimer = Timer.CreateNew(gameObject, "Duration Timer");
+        durationTimer.Setup(1f);
+        durationTimer.StartTimer();
+
+        repeaterTimer = Timer.CreateNew(gameObject, "Repeater Timer");
+        repeaterTimer.SetupRepeater(2f, true, 5);
+        repeaterTimer.StartTimer();
+        
+        intervalTimer = Timer.CreateNew(gameObject, "Interval Timer");
+        intervalTimer.SetupInterval(0f, 1f, false, 5);
+        intervalTimer.StartTimer();
+        
+
+        reset_btn.onClick.AddListener(() =>
+        {
+            durationTimer.StartTimer();
+            repeaterTimer.StartTimer();
+            intervalTimer.StartTimer();
+
+        });
+        startstop_btn.onClick.AddListener(() =>
+        {
+            durationTimer.PauseUnpauseTimer();
+            repeaterTimer.PauseUnpauseTimer();
+            intervalTimer.PauseUnpauseTimer();
+        });
+        shoot_btn.onClick.AddListener(() =>
+        {
+            Debug.Log($"durationTimer Can Shoot: {durationTimer.TimerHit}");
+            Debug.Log($"repeaterTimer Can Shoot: {repeaterTimer.TimerHit}");
+            Debug.Log($"intervalTimer Can Shoot: {intervalTimer.TimerHit}");
+        });
+    }
+
+    void Update()
+    {
+        if (durationTimer)
+        {
+            var running = durationTimer.IsRuning.ToString();
+            var time = durationTimer.TotalTime;
+            var runs = durationTimer.TotalRuns;
+            durationRunning_Text.text = $"Running = {running}";
+            durationTime_Text.text = $"Time = {time}";
+            durationRuns_Text.text = $"Runs = {runs}";
+        }
+
+        if (repeaterTimer)
+        {
+            var running = repeaterTimer.IsRuning.ToString();
+            var time = repeaterTimer.TotalTime;
+            var runs = repeaterTimer.TotalRuns;
+            repeaterRunning_Text.text = $"Running = {running}";
+            repeaterTime_Text.text = $"Time = {time}";
+            repeaterRuns_Text.text = $"Runs = {runs}";
+        }
+
+        if (intervalTimer)
+        {
+            var running = intervalTimer.IsRuning.ToString();
+            var time = intervalTimer.TotalTime;
+            var runs = intervalTimer.TotalRuns;
+            intervalRunning_Text.text = $"Running = {running}";
+            intervalTime_Text.text = $"Time = {time}";
+            intervalRuns_Text.text = $"Runs = {runs}";
+        }
+    }
+}
---
+++ Timer/Timer.cs
@@ -0,0 +1,171 @@
+﻿/*
+ * About:
+ * Simple Timer class
+ *
+ * Features:
+ * 3 Timer setup types : Duration, Repeater, Interval
+ *
+ * How To Use:
+ * 1. Create a timer using Timer.CreateNew()
+ *      e.g. var timer = Timer.CreateNew(gameObject);
+ *
+ * 2. Setup Timer using Setup(), SetupRepeater() or SetupInterval()
+ *      e.g. timer.Setup(1);
+ *
+ * 3. Use timer.TimerHit property to check and do something about it
+ *      e.g. if(timer.TimerHit) Shoot();
+ *
+ * How It Works:
+ * Uses Unity Engine Update Loop
+ *
+ * Notes:
+ * No Start Delay
+ */
+
+using UnityEngine;
+
+namespace asim.unity.utils
+{
+    public class Timer : MonoBehaviour
+    {
+        private bool autoContinueRun;
+        private bool currentRunEnded;
+
+        private int maxRuns;
+        private float timerEnd;
+
+        private float timerStart;
+
+        /* Public Accessors */
+        public bool IsRuning { get; private set; }
+
+        public bool TimerHit
+        {
+            get
+            {
+                var hit = IsRuning && currentRunEnded;
+
+                if (!autoContinueRun && currentRunEnded)
+                {
+                    Continue();
+                    currentRunEnded = false;
+                }
+
+                return hit;
+            }
+        }
+
+        public float CurrentTimer { get; private set; }
+
+        public float TotalTime { get; private set; }
+
+        public int TotalRuns { get; private set; }
+
+        /* Private */
+        private void Reset()
+        {
+            CurrentTimer = timerStart;
+            TotalTime = 0;
+            TotalRuns = 0;
+            currentRunEnded = false;
+        }
+
+        private void Update()
+        {
+            if (!IsRuning) return;
+
+            if (currentRunEnded)
+            {
+                if (TotalRuns >= maxRuns)
+                {
+                    IsRuning = false;
+                    return;
+                }
+
+                if (autoContinueRun)
+                {
+                    currentRunEnded = false;
+                    Continue();
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            CurrentTimer += Time.deltaTime;
+            TotalTime += Time.deltaTime;
+            if (CurrentTimer >= timerEnd)
+            {
+                TotalRuns++;
+                currentRunEnded = true;
+            }
+        }
+
+        private void Continue()
+        {
+            CurrentTimer = timerStart;
+        }
+
+        /* Public */
+        public static Timer CreateNew(GameObject gameObject, string name = null)
+        {
+            if (string.IsNullOrEmpty(name)) name = $"{gameObject.name} timer";
+
+            var newGO = new GameObject(name, typeof(Timer));
+            newGO.transform.parent = gameObject.transform;
+            var timer = newGO.GetComponent<Timer>();
+
+            return timer;
+        }
+
+        /// <summary>
+        ///     Simple timer that counts down from a set duration.
+        /// </summary>
+        public void Setup(float duration)
+        {
+            timerStart = 0;
+            timerEnd = duration;
+            maxRuns = 1;
+            autoContinueRun = false;
+        }
+
+        /// <summary>
+        ///     Timer that counts down and automatically repeats
+        /// </summary>
+        public void SetupRepeater(float duration, bool autoContinueRun = true, int maxRuns = int.MaxValue)
+        {
+            timerStart = 0;
+            timerEnd = duration;
+            this.maxRuns = maxRuns;
+            this.autoContinueRun = autoContinueRun;
+        }
+
+        /// <summary>
+        ///     Setup a custom timer
+        /// </summary>
+        public void SetupInterval(float start, float end, bool autoContinueRun = true, int maxRuns = int.MaxValue)
+        {
+            timerStart = start;
+            timerEnd = end;
+            this.maxRuns = maxRuns;
+            this.autoContinueRun = autoContinueRun;
+        }
+
+        public void StartTimer()
+        {
+            IsRuning = true;
+            Reset();
+        }
+
+        public void StopTimer()
+        {
+            IsRuning = false;
+        }
+
+        public void PauseUnpauseTimer()
+        {
+            IsRuning = !IsRuning;
+        }
+    }
+}
